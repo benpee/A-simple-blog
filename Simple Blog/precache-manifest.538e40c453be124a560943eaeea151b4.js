7ac756306edd235d69539a781a7ebd0095ed92b4
benpee <74276700+benpee@users.noreply.github.com>
2020-12-20 06:09:38 +0100

    Add files via upload

---
+++ Simple Blog/precache-manifest.538e40c453be124a560943eaeea151b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e041adb55fc1665c0fddd057031f11ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e2a62d40f619ae5cda1",
+    "url": "/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "d835261a14835314733c",
+    "url": "/static/css/main.dc7123b6.chunk.css"
+  },
+  {
+    "revision": "3e2a62d40f619ae5cda1",
+    "url": "/static/js/2.53eda4f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.53eda4f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d835261a14835314733c",
+    "url": "/static/js/main.9697e960.chunk.js"
+  },
+  {
+    "revision": "2ae6e346d186b5e5b788",
+    "url": "/static/js/runtime-main.ca8a46de.js"
+  }
+]);
