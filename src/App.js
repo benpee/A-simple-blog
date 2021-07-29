2f73a8731b79a418466437c395ed531b403f0eec
Ajibola <poyeyemi@gmail.com>
2021-07-29 15:37:38 +0100

    new changes

---
+++ src/App.js
@@ -0,0 +1,12 @@
+﻿import React from 'react';
+import BlogSite from './BlogSite';
+
+function App() {
+  return (
+    <div className="">
+      <BlogSite />
+    </div>
+  );
+} 
+
+export default App;
